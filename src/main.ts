66f16948c173420b4b582e88347b2bf79815fb3d
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2023-09-04 14:28:06 +0100

    Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+                    .setTitle('Locket API')
+                    .setDescription('An API to replicate the locket app functionalites for sharing pictures with friends')
+                    .setVersion('1.0')
+                    .build();
+
+  const document = SwaggerModule.createDocument(app,  config);
+
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
