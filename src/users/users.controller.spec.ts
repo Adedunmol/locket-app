38a292d70c3deb34cfcacaed062cfcbe935c5d97
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2023-09-07 18:25:48 +0100

    Add tests for users service and controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,11 +5,20 @@ import { UsersService } from './users.service';
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    create: jest.fn(dto => {
+      return {
+        id: Date.now(),
+        ...dto
+      }
+    })
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    }).overrideProvider(UsersService).useValue(mockUsersService).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
@@ -17,4 +26,17 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a user', () => {
+    const dto = { country_code: '+234', phone_number: '1234567890', password: 'password' }
+
+    expect(controller.create(dto)).toEqual({
+      id: expect.any(Number),
+      country_code: dto.country_code,
+      phone_number: dto.phone_number,
+      password: dto.password
+    })
+
+    expect(mockUsersService.create).toHaveBeenCalled()
+  })
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const currentDate = new Date();
+
+  const mockUsersRepository = {
+    create: jest.fn(dto => dto),
+    save: jest.fn(user => Promise.resolve({ id: Date.now(), date_joined: currentDate, ...user })),
+    findByNumber: jest.fn(phone_number => null),
+    findOne: jest.fn(query => null)
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, {
+        provide: getRepositoryToken(User),
+        useValue: mockUsersRepository
+      }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,4 +29,14 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a new user record and return the new record', async () => {
+    const dto = { password: 'password12', phone_number: '1234567890', country_code: '+234' }
+    expect(await service.create(dto)).toEqual({
+      id: expect.any(Number),
+      phone_number: dto.phone_number,
+      country_code: dto.country_code,
+      date_joined: currentDate
+    })
+  })
 });
